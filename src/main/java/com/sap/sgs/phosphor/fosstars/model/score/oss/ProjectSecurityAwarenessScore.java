4fd4d96464f808bffc56b9c40810253f396e253d
D067452 <sourabh.sarvotham.parkala@sap.com>
2020-03-17 09:04:16 +0100

    Update ProjectSecurityAwarenessScore to use USES_VERIFIED_SIGNED_COMMITS
    - Refactoring ProjectSecurityAwarenessScore.java to also consider USES_VERIFIED_SIGNED_COMMITS for score calculation
    
    This is a part of #54

--- src/main/java/com/sap/sgs/phosphor/fosstars/model/score/oss/ProjectSecurityAwarenessScore.java
+++ src/main/java/com/sap/sgs/phosphor/fosstars/model/score/oss/ProjectSecurityAwarenessScore.java
@@ -2,6 +2,7 @@ package com.sap.sgs.phosphor.fosstars.model.score.oss;
 
 import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.HAS_SECURITY_POLICY;
 import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.HAS_SECURITY_TEAM;
+import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.USES_VERIFIED_SIGNED_COMMITS;
 import static com.sap.sgs.phosphor.fosstars.model.other.Utils.findValue;
 
 import com.sap.sgs.phosphor.fosstars.model.Confidence;
@@ -19,6 +20,7 @@ import java.util.List;
  * <ul>
  *   <li>if a project publishes security advisories</li>
  *   <li>if a project has a security team</li>
+ *   <li>if a project uses verified signed commits</li>
  * </ul>
  * A project gets the maximum score if it has both a security policy and a security team.
  */
@@ -27,25 +29,31 @@ public class ProjectSecurityAwarenessScore extends FeatureBasedScore {
   /**
    * A number of points which are added to a score value if a project has a security policy.
    */
-  private static final double SECURITY_POLICY_POINTS = 5.0;
+  private static final double SECURITY_POLICY_POINTS = 3.0;
 
   /**
    * A number of points which are added to a score value if a project has a security team.
    */
-  private static final double SECURITY_TEAM_POINTS = 8.0;
+  private static final double SECURITY_TEAM_POINTS = 5.0;
+
+  /**
+   * A number of points which are added to a score value if a project uses verified signed commits.
+   */
+  private static final double SIGNED_COMMITS_POINTS = 2.0;
 
   private static final String DESCRIPTION = String.format(
       "The score checks if a project has a security policy and a security team.\n"
           + "If the project has a security policy, then the score adds %2.2f.\n"
-          + "If the project has a security team, then the score adds %2.2f.",
-      SECURITY_POLICY_POINTS, SECURITY_TEAM_POINTS);
+          + "If the project has a security team, then the score adds %2.2f.\n"
+          + "If the project uses verified signed commits, then the score adds %2.2f.",
+      SECURITY_POLICY_POINTS, SECURITY_TEAM_POINTS, SIGNED_COMMITS_POINTS);
 
   /**
    * Initializes a new {@link ProjectSecurityAwarenessScore}.
    */
   ProjectSecurityAwarenessScore() {
     super("How well open-source community is aware about security", DESCRIPTION,
-        HAS_SECURITY_POLICY, HAS_SECURITY_TEAM);
+        HAS_SECURITY_POLICY, HAS_SECURITY_TEAM, USES_VERIFIED_SIGNED_COMMITS);
   }
 
   @Override
@@ -54,6 +62,8 @@ public class ProjectSecurityAwarenessScore extends FeatureBasedScore {
         "Hey! You have to tell me if the project has a security policy!");
     Value<Boolean> securityTeam = findValue(values, HAS_SECURITY_TEAM,
         "Hey! You have to tell me if the project has a security team!");
+    Value<Boolean> signedCommits = findValue(values, USES_VERIFIED_SIGNED_COMMITS,
+        "Hey! You have to tell me if the project uses verified signed commits!");
 
     ScoreValue scoreValue = new ScoreValue(this);
     securityPolicy.processIfKnown(exist -> {
@@ -68,8 +78,14 @@ public class ProjectSecurityAwarenessScore extends FeatureBasedScore {
       }
     });
 
-    scoreValue.usedValues(securityPolicy, securityTeam);
-    scoreValue.confidence(Confidence.make(securityPolicy, securityTeam));
+    signedCommits.processIfKnown(exist -> {
+      if (exist) {
+        scoreValue.increase(SIGNED_COMMITS_POINTS);
+      }
+    });
+
+    scoreValue.usedValues(securityPolicy, securityTeam, signedCommits);
+    scoreValue.confidence(Confidence.make(securityPolicy, securityTeam, signedCommits));
 
     return scoreValue;
   }
