072e7a5ef59854d80a25c502f44013a4597c4bd1
D067452 <sourabh.sarvotham.parkala@sap.com>
2020-03-17 09:06:01 +0100

    Update tests to reflect ProjectSecurityAwarenessScore changes
    - Updated ProjectSecurityAwarenessScoreTestVectors.ipynb to use USES_VERIFIED_SIGNED_COMMITS as the new feature.
    - Generated new range of test vectors which are reflected in ProjectSecurityAwarenessScoreTestVectors.csv
    - Removing a duplicate test case which is already being handled as part of the test vector test run ProjectSecurityAwarenessScoreVerification
    
    This is a part of #54

--- src/test/java/com/sap/sgs/phosphor/fosstars/model/score/oss/ProjectSecurityAwarenessScoreTest.java
+++ src/test/java/com/sap/sgs/phosphor/fosstars/model/score/oss/ProjectSecurityAwarenessScoreTest.java
@@ -1,14 +1,11 @@
 package com.sap.sgs.phosphor.fosstars.model.score.oss;
 
-import static com.sap.sgs.phosphor.fosstars.TestUtils.assertScore;
 import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.HAS_SECURITY_POLICY;
 import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.HAS_SECURITY_TEAM;
-import static com.sap.sgs.phosphor.fosstars.model.other.Utils.setOf;
+import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.USES_VERIFIED_SIGNED_COMMITS;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
-import com.sap.sgs.phosphor.fosstars.model.Score;
-import com.sap.sgs.phosphor.fosstars.model.value.UnknownValue;
 import org.junit.Test;
 
 public class ProjectSecurityAwarenessScoreTest {
@@ -19,59 +16,19 @@ public class ProjectSecurityAwarenessScoreTest {
   @Test(expected = IllegalArgumentException.class)
   public void noInfoAboutSecurityAdvisories() {
     PROJECT_SECURITY_AWARENESS.calculate(HAS_SECURITY_TEAM.value(false));
+    PROJECT_SECURITY_AWARENESS.calculate(USES_VERIFIED_SIGNED_COMMITS.value(false));
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void noInfoAboutSecurityTeam() {
     PROJECT_SECURITY_AWARENESS.calculate(HAS_SECURITY_POLICY.value(false));
+    PROJECT_SECURITY_AWARENESS.calculate(USES_VERIFIED_SIGNED_COMMITS.value(false));
   }
 
-  @Test
-  public void test() {
-    assertScore(Score.MIN,
-        PROJECT_SECURITY_AWARENESS, setOf(
-            UnknownValue.of(HAS_SECURITY_TEAM),
-            UnknownValue.of(HAS_SECURITY_POLICY)));
-
-    assertScore(Score.MIN,
-        PROJECT_SECURITY_AWARENESS, setOf(
-            HAS_SECURITY_TEAM.value(false),
-            HAS_SECURITY_POLICY.value(false)));
-
-    assertScore(Score.MIN,
-        PROJECT_SECURITY_AWARENESS, setOf(
-            UnknownValue.of(HAS_SECURITY_TEAM),
-            HAS_SECURITY_POLICY.value(false)));
-
-    assertScore(Score.MIN,
-        PROJECT_SECURITY_AWARENESS, setOf(
-            HAS_SECURITY_TEAM.value(false),
-            UnknownValue.of(HAS_SECURITY_POLICY)));
-
-    assertScore(5.0,
-        PROJECT_SECURITY_AWARENESS, setOf(
-            UnknownValue.of(HAS_SECURITY_TEAM),
-            HAS_SECURITY_POLICY.value(true)));
-
-    assertScore(8.0,
-        PROJECT_SECURITY_AWARENESS, setOf(
-            HAS_SECURITY_TEAM.value(true),
-            UnknownValue.of(HAS_SECURITY_POLICY)));
-
-    assertScore(8.0,
-        PROJECT_SECURITY_AWARENESS, setOf(
-            HAS_SECURITY_TEAM.value(true),
-            HAS_SECURITY_POLICY.value(false)));
-
-    assertScore(5.0,
-        PROJECT_SECURITY_AWARENESS, setOf(
-            HAS_SECURITY_TEAM.value(false),
-            HAS_SECURITY_POLICY.value(true)));
-
-    assertScore(Score.MAX,
-        PROJECT_SECURITY_AWARENESS, setOf(
-            HAS_SECURITY_TEAM.value(true),
-            HAS_SECURITY_POLICY.value(true)));
+  @Test(expected = IllegalArgumentException.class)
+  public void noInfoAboutSignedCommits() {
+    PROJECT_SECURITY_AWARENESS.calculate(HAS_SECURITY_POLICY.value(false));
+    PROJECT_SECURITY_AWARENESS.calculate(HAS_SECURITY_TEAM.value(false));
   }
 
   @Test
