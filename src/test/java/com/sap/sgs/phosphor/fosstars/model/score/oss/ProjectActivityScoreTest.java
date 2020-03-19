530f842974ff02f8fded38b16e4e05d1e639af75
Artem Smotrakov <artem.smotrakov@sap.com>
2020-03-19 13:41:35 +0100

    Adjusted ProjectActivityScore
    - Updated the logistic functions which are used by the score
    - Contributors now contribute more to the score
    - Updated the test vectors for the score
    - Updated ProjectActivityScoreTest
    
    This addresses #19

--- src/test/java/com/sap/sgs/phosphor/fosstars/model/score/oss/ProjectActivityScoreTest.java
+++ src/test/java/com/sap/sgs/phosphor/fosstars/model/score/oss/ProjectActivityScoreTest.java
@@ -4,9 +4,6 @@ import static com.sap.sgs.phosphor.fosstars.TestUtils.assertScore;
 import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.NUMBER_OF_COMMITS_LAST_THREE_MONTHS;
 import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.NUMBER_OF_CONTRIBUTORS_LAST_THREE_MONTHS;
 import static com.sap.sgs.phosphor.fosstars.model.other.Utils.setOf;
-import static com.sap.sgs.phosphor.fosstars.model.score.oss.ProjectActivityScore.LOGISTIC_FOR_NUMBER_OF_COMMITS;
-import static com.sap.sgs.phosphor.fosstars.model.score.oss.ProjectActivityScore.LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
@@ -19,25 +16,6 @@ import org.junit.Test;
 public class ProjectActivityScoreTest {
 
   private static final ProjectActivityScore PROJECT_ACTIVITY = new ProjectActivityScore();
-  private static final double delta = 0.01;
-
-  @Test
-  public void logisticForNumberOfCommits() {
-    assertEquals(0.84, LOGISTIC_FOR_NUMBER_OF_COMMITS.value(0), delta);
-    assertEquals(1.19, LOGISTIC_FOR_NUMBER_OF_COMMITS.value(10), delta);
-    assertEquals(3.55, LOGISTIC_FOR_NUMBER_OF_COMMITS.value(50), delta);
-    assertEquals(5.45, LOGISTIC_FOR_NUMBER_OF_COMMITS.value(80), delta);
-    assertEquals(6.25, LOGISTIC_FOR_NUMBER_OF_COMMITS.value(100), delta);
-  }
-
-  @Test
-  public void logisticForNumberOfContributors() {
-    assertEquals(1.39, LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS.value(0), delta);
-    assertEquals(1.55, LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS.value(1), delta);
-    assertEquals(2.14, LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS.value(5), delta);
-    assertEquals(2.66, LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS.value(10), delta);
-    assertEquals(3.03, LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS.value(20), delta);
-  }
 
   @Test
   public void calculate() {
@@ -54,33 +32,6 @@ public class ProjectActivityScoreTest {
             NUMBER_OF_CONTRIBUTORS_LAST_THREE_MONTHS.value(0)));
 
     assertScore(Score.MIN, PROJECT_ACTIVITY, values(0, 0));
-
-    // for one contributor
-    assertScore(2.42, PROJECT_ACTIVITY, values(1, 1));
-    assertScore(2.74, PROJECT_ACTIVITY, values(10, 1));
-    assertScore(3.75, PROJECT_ACTIVITY, values(30, 1));
-    assertScore(6.44, PROJECT_ACTIVITY, values(70, 1));
-    assertScore(7.80, PROJECT_ACTIVITY, values(100, 1));
-    assertScore(8.63, PROJECT_ACTIVITY, values(200, 1));
-
-    // for more many contributors
-    assertScore(2.89, PROJECT_ACTIVITY, values(10, 2));
-    assertScore(3.90, PROJECT_ACTIVITY, values(30, 2));
-    assertScore(6.60, PROJECT_ACTIVITY, values(70, 2));
-    assertScore(7.95, PROJECT_ACTIVITY, values(100, 2));
-    assertScore(8.78, PROJECT_ACTIVITY, values(200, 2));
-
-    // for more many contributors
-    assertScore(3.33, PROJECT_ACTIVITY, values(10, 5));
-    assertScore(4.34, PROJECT_ACTIVITY, values(30, 5));
-    assertScore(7.03, PROJECT_ACTIVITY, values(70, 5));
-    assertScore(8.39, PROJECT_ACTIVITY, values(100, 5));
-    assertScore(9.22, PROJECT_ACTIVITY, values(200, 5));
-    assertScore(3.85, PROJECT_ACTIVITY, values(10, 10));
-    assertScore(4.86, PROJECT_ACTIVITY, values(30, 10));
-    assertScore(7.55, PROJECT_ACTIVITY, values(70, 10));
-    assertScore(8.91, PROJECT_ACTIVITY, values(100, 10));
-    assertScore(9.74, PROJECT_ACTIVITY, values(200, 10));
 
     // I wish all open-source projects were like that
     assertScore(Score.MAX, PROJECT_ACTIVITY, values(10000, 500));
