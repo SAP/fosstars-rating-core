cf32bde6fa5c36007c0445f3443280c2a0d6b2b6
Artem Smotrakov <artem.smotrakov@sap.com>
2020-03-19 13:41:35 +0100

    Adjusted ProjectActivityScore
    - Updated the logistic functions which are used by the score
    - Contributors now contribute more to the score
    - Updated the test vectors for the score
    - Updated ProjectActivityScoreTest
    
    This addresses #19

--- src/main/java/com/sap/sgs/phosphor/fosstars/model/score/oss/ProjectActivityScore.java
+++ src/main/java/com/sap/sgs/phosphor/fosstars/model/score/oss/ProjectActivityScore.java
@@ -12,7 +12,10 @@ import com.sap.sgs.phosphor.fosstars.model.score.FeatureBasedScore;
 import com.sap.sgs.phosphor.fosstars.model.value.ScoreValue;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import org.apache.commons.math3.analysis.UnivariateFunction;
 import org.apache.commons.math3.analysis.function.Logistic;
 
 /**
@@ -26,45 +29,76 @@ import org.apache.commons.math3.analysis.function.Logistic;
 public class ProjectActivityScore extends FeatureBasedScore {
 
   /**
-   * y(n) = 7.1 / (1 + e ^ (0.04 * (50 - n))),
-   * where n is a number of commits in the last three months.
-   * y(n) belongs to the interval (0.8, 7.1) when n > 0
-   * Number of commits can contribute up to 7 to the overall project activity score.
+   * The maximum sub-score based on a number of commits in the last three month.
    */
-  static final Logistic LOGISTIC_FOR_NUMBER_OF_COMMITS
-      = new Logistic(7.1, 50, 0.04, 1, 0, 1);
+  private static final double MAX_COMMITS_SUB_SCORE = 5.1;
 
   /**
-   * y(m) = 3.1 / (1 + e ^ (0.2 * (1 - m))),
-   * where m is a number of contributors in the last three months.
-   * y(m) belongs to the interval (1.3, 3.1) when m > 0
-   * Number of contributors can contribute up to 3 to the overall project activity score.
+   * A logistic function which is used to convert a number of commits to a sub-score:
+   * <pre>y(n) = MAX_COMMITS_SUB_SCORE / (1 + e ^ (0.02 * (200 - n)))</pre>,
+   * where n is a number of commits in the last three months.
+   * The function belongs to the interval [0.0, MAX_COMMITS_SUB_SCORE) when n >= 0.
    */
-  static final Logistic LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS
-      = new Logistic(3.1, 1, 0.2, 1, 0, 1);
+  private static final Logistic LOGISTIC_FOR_NUMBER_OF_COMMITS
+      = logistic(MAX_COMMITS_SUB_SCORE, 0.02, 200);
 
+  /**
+   * The maximum sub-score based on a number of contributors in the last three months.
+   */
+  private static final double MAX_CONTRIBUTORS_SUB_SCORE = 5.1;
+
+  /**
+   * A shift on the Y-axis for the logistic function for the number of contributors.
+   */
+  private static final double MAX_CONTRIBUTORS_SUB_SCORE_SHIFT = -1;
+
+  /**
+   * A logistic function which is used to convert a number of contributors to a sub-score:
+   * <pre>y(m) = A / (1 + e ^ (0.4 * (4 - m)))</pre>,
+   * where m is a number of contributors in the last three months,
+   * and A = MAX_CONTRIBUTORS_SUB_SCORE - MAX_CONTRIBUTORS_SUB_SCORE_SHIFT.
+   * The function belongs to the interval [0.0, MAX_CONTRIBUTORS_SUB_SCORE) when m >= 0.
+   */
+  private static final Logistic LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS
+      = logistic(MAX_CONTRIBUTORS_SUB_SCORE - MAX_CONTRIBUTORS_SUB_SCORE_SHIFT, 0.4, 4);
+
+  /**
+   * A final function which is used to convert a number of contributors to a sub-score:
+   * <pre>y(m) = LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS(m) - MAX_CONTRIBUTORS_SUB_SCORE_SHIFT</pre>.
+   */
+  private static final UnivariateFunction NUMBER_OF_CONTRIBUTORS_SUB_SCORE =
+      m -> LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS.value(m) + MAX_CONTRIBUTORS_SUB_SCORE_SHIFT;
+
+  /**
+   * Returns a logistic function y(x) = a / (1 + e ^ k(b - x)).
+   */
+  private static Logistic logistic(double a, double k, double b) {
+    return new Logistic(a, b, k, 1, 0, 1);
+  }
+
+  /**
+   * A description of the score.
+   */
   private static final String DESCRIPTION;
 
   static {
     StringBuilder sb = new StringBuilder();
 
     sb.append("The score is based on number of commits and contributors.\n");
-    sb.append("Here is how a number of commits contributes to the score:\n");
-    sb.append(String.format("%d -> %2.2f (min), ", 0, LOGISTIC_FOR_NUMBER_OF_COMMITS.value(0)));
-    sb.append(String.format("%d -> %2.2f, %d -> %2.2f, ",
-        MathHelper.invert(LOGISTIC_FOR_NUMBER_OF_COMMITS, 0, 1000, 2.5, 0.01), 2.5,
-        MathHelper.invert(LOGISTIC_FOR_NUMBER_OF_COMMITS, 0, 1000, 5.0, 0.01), 5.0));
-    sb.append(String.format("%d -> %2.2f (max)", 500, LOGISTIC_FOR_NUMBER_OF_COMMITS.value(500)));
+    sb.append(String.format(
+        "Here is how the number of commits contributes to the score (up to %2.2f):\n",
+        MAX_COMMITS_SUB_SCORE));
+    sb.append(printInvertedValues(
+        LOGISTIC_FOR_NUMBER_OF_COMMITS::value, 0, 1000,
+        0.1, MAX_COMMITS_SUB_SCORE * 0.5, MAX_COMMITS_SUB_SCORE * 0.9));
 
     sb.append("\n");
-    sb.append("Here is how a number of contributors contributes to the score:\n");
-    sb.append(String.format("%d -> %2.2f (min), ",
-        0, LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS.value(0)));
-    sb.append(String.format("%d -> %2.2f, %d -> %2.2f, ",
-        MathHelper.invert(LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS, 0, 50, 1.5, 0.01), 1.5,
-        MathHelper.invert(LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS, 0, 100, 2.5, 0.01), 2.5));
-    sb.append(String.format("%d -> %2.2f (max)",
-        500, LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS.value(500)));
+    sb.append(String.format(
+        "Here is how the number of contributors contributes to the score (up to %2.2f):\n",
+        MAX_CONTRIBUTORS_SUB_SCORE));
+    sb.append(printInvertedValues(
+        NUMBER_OF_CONTRIBUTORS_SUB_SCORE::value, 0, 200,
+        0.1, MAX_CONTRIBUTORS_SUB_SCORE * 0.5, MAX_CONTRIBUTORS_SUB_SCORE * 0.9));
 
     DESCRIPTION = sb.toString();
   }
@@ -95,18 +129,18 @@ public class ProjectActivityScore extends FeatureBasedScore {
    * @param numberOfCommits The number of commits.
    * @return The sub-score.
    */
-  private double commitsPoints(Value<Integer> numberOfCommits) {
+  private static double commitsPoints(Value<Integer> numberOfCommits) {
     if (numberOfCommits.isUnknown()) {
-      return 0;
+      return 0.0;
     }
 
     int n = numberOfCommits.get();
-    if (numberOfCommits.get() == 0) {
-      return 0;
-    }
     if (n < 0) {
       throw new IllegalArgumentException(
           "Hey! You are're not supposed to give me a negative number of commits!");
+    }
+    if (n == 0) {
+      return 0.0;
     }
 
     return LOGISTIC_FOR_NUMBER_OF_COMMITS.value(n);
@@ -118,18 +152,18 @@ public class ProjectActivityScore extends FeatureBasedScore {
    * @param numberOfContributors The number of contributors.
    * @return The sub-score value.
    */
-  private double contributorsPoints(Value<Integer> numberOfContributors) {
+  private static double contributorsPoints(Value<Integer> numberOfContributors) {
     if (numberOfContributors.isUnknown()) {
-      return 0;
+      return 0.0;
     }
 
     int n = numberOfContributors.get();
-    if (numberOfContributors.get() == 0) {
-      return 0;
-    }
     if (n < 0) {
       throw new IllegalArgumentException(
           "Hey! You are're not supposed to give me a negative number of contributors!");
+    }
+    if (n == 0) {
+      return 0;
     }
 
     return LOGISTIC_FOR_NUMBER_OF_CONTRIBUTORS.value(n);
@@ -157,6 +191,26 @@ public class ProjectActivityScore extends FeatureBasedScore {
     if (n > 0 && m == 0) {
       throw new IllegalArgumentException("Commits without contributor! How is that possible?");
     }
+  }
+
+  /**
+   * Inverts a number of values of a function on a specified interval and prints them out.
+   *
+   * @param func The function to be inverted.
+   * @param a A beginning of the interval.
+   * @param b A end of the interval.
+   * @param values The values of the function to be inverted.
+   * @return A string with inverted values.
+   */
+  private static String printInvertedValues(
+      Function<Integer, Double> func, int a, int b, double... values) {
+
+    List<String> strings = new ArrayList<>();
+    for (double value : values) {
+      strings.add(String.format("%d -> %2.2f",
+          MathHelper.invert(func, a, b, value, 0.01), value));
+    }
+    return String.join(", ", strings);
   }
 
   /**
