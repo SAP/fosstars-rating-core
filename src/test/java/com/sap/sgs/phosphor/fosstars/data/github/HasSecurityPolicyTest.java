020aa6255bfbfe9644a16dfa7df387714fdc18ae
Artem Smotrakov <artem.smotrakov@sap.com>
2020-04-21 19:04:28 +0200

    Updated HasSecurityPolicyTest to use mocks
    This fixes #9

--- src/test/java/com/sap/sgs/phosphor/fosstars/data/github/HasSecurityPolicyTest.java
+++ src/test/java/com/sap/sgs/phosphor/fosstars/data/github/HasSecurityPolicyTest.java
@@ -1,73 +1,80 @@
 package com.sap.sgs.phosphor.fosstars.data.github;
 
-import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.SCANS_FOR_VULNERABLE_DEPENDENCIES;
+import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.HAS_SECURITY_POLICY;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import com.sap.sgs.phosphor.fosstars.data.Terminal;
 import com.sap.sgs.phosphor.fosstars.model.Value;
 import com.sap.sgs.phosphor.fosstars.model.value.ValueHashSet;
+import com.sap.sgs.phosphor.fosstars.tool.github.GitHubDataFetcher;
 import com.sap.sgs.phosphor.fosstars.tool.github.GitHubProject;
+import com.sap.sgs.phosphor.fosstars.tool.github.GitHubProjectValueCache;
 import java.io.IOException;
 import java.util.Optional;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.kohsuke.github.GHContent;
+import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
-/**
- * TODO: This test fails when GitHub API rate limit exceeded or some networking issues occur.
- *       The test needs to be re-written to use mocks.
- *       Meanwhile, the test is ignored.
- */
-@Ignore
 public class HasSecurityPolicyTest {
 
-  private static GitHub github;
-
-  @BeforeClass
-  public static void initGithubConnection() {
-    try {
-      github = GitHub.connectAnonymously();
-      github.checkApiUrlValidity();
-    } catch (Exception e) {
-      System.out.println("Couldn't connect to GitHub!");
-      e.printStackTrace();
-      github = null;
-    }
+  @Before
+  @After
+  public void cleanup() {
+    GitHubDataFetcher.instance().repositoryCache().clear();
+    GitHubDataFetcher.instance().commitsCache().clear();
   }
 
   @Test
-  public void testApacheNifi() throws IOException {
-    assumeNotNull(github);
+  public void testHasPolicy() throws IOException {
+    GHContent content = mock(GHContent.class);
+    when(content.isFile()).thenReturn(true);
+    when(content.getSize()).thenReturn(1000L);
+
+    GHRepository repository = mock(GHRepository.class);
+    when(repository.getFileContent("SECURITY.md")).thenReturn(content);
+
+    GitHub github = mock(GitHub.class);
+    when(github.getRepository(any())).thenReturn(repository);
+
     HasSecurityPolicy provider = new HasSecurityPolicy(github);
+    provider.set(new GitHubProjectValueCache());
+
+    check(provider, true);
+  }
+
+  @Test
+  public void testNoPolicy() throws IOException {
+    GHRepository repository = mock(GHRepository.class);
+    when(repository.getFileContent("SECURITY.md")).thenThrow(IOException.class);
+
+    GitHub github = mock(GitHub.class);
+    when(github.getRepository(any())).thenReturn(repository);
+
+    HasSecurityPolicy provider = new HasSecurityPolicy(github);
+    provider.set(new GitHubProjectValueCache());
+
+    check(provider, false);
+  }
+
+  private static void check(HasSecurityPolicy provider, boolean expectedValue) throws IOException {
     ValueHashSet values = new ValueHashSet();
-    GitHubProject project = new GitHubProject("apache", "nifi");
-    provider.set(new Terminal()).update(project, values);
-    assertTrue(values.has(SCANS_FOR_VULNERABLE_DEPENDENCIES));
-    Optional<Value> something = values.of(SCANS_FOR_VULNERABLE_DEPENDENCIES);
+    GitHubProject project = new GitHubProject("org", "test");
+    provider.update(project, values);
+
+    assertEquals(1, values.size());
+    assertTrue(values.has(HAS_SECURITY_POLICY));
+    Optional<Value> something = values.of(HAS_SECURITY_POLICY);
     assertNotNull(something);
     assertTrue(something.isPresent());
     Value<Boolean> value = something.get();
     assertNotNull(value);
-    assertEquals(Boolean.TRUE, value.get());
-  }
-
-  @Test
-  public void testApacheCxf() throws IOException {
-    assumeNotNull(github);
-    GitHubProject project = new GitHubProject("apache", "cxf");
-    HasSecurityPolicy provider = new HasSecurityPolicy(github);
-    ValueHashSet values = new ValueHashSet();
-    provider.set(new Terminal()).update(project, values);
-    assertTrue(values.has(SCANS_FOR_VULNERABLE_DEPENDENCIES));
-    Optional<Value> something = values.of(SCANS_FOR_VULNERABLE_DEPENDENCIES);
-    assertNotNull(something);
-    assertTrue(something.isPresent());
-    Value<Boolean> value = something.get();
-    assertNotNull(value);
-    assertEquals(Boolean.FALSE, value.get());
+    assertEquals(expectedValue, value.get());
   }
 }
