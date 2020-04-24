011ad10f9079d1e9a9dd2bc004b4dd5e30b656ca
Artem Smotrakov <artem.smotrakov@sap.com>
2020-04-24 16:27:07 +0200

    Make UsesGithubForDevelopment data provider smarter
    - Updated the UsesGithubForDevelopment data provider to consider
      multiple facts about a project
    - Updated UsesGithubForDevelopmentTest
    
    This fixes #122

--- src/test/java/com/sap/sgs/phosphor/fosstars/data/github/UsesGithubForDevelopmentTest.java
+++ src/test/java/com/sap/sgs/phosphor/fosstars/data/github/UsesGithubForDevelopmentTest.java
@@ -1,5 +1,6 @@
 package com.sap.sgs.phosphor.fosstars.data.github;
 
+import static com.sap.sgs.phosphor.fosstars.data.github.UsesGithubForDevelopment.CONFIDENCE_THRESHOLD;
 import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.USES_GITHUB_FOR_DEVELOPMENT;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -12,98 +13,133 @@ import com.sap.sgs.phosphor.fosstars.model.value.ValueHashSet;
 import com.sap.sgs.phosphor.fosstars.tool.github.GitHubProject;
 import com.sap.sgs.phosphor.fosstars.tool.github.GitHubProjectValueCache;
 import java.io.IOException;
-import org.apache.commons.lang3.StringUtils;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.function.Consumer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.kohsuke.github.GHCommit;
 import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHUser;
 import org.kohsuke.github.GitHub;
 
 public class UsesGithubForDevelopmentTest {
 
-  @Test
-  public void testWithMirrorUrl() throws IOException {
-    GitHub github = mock(GitHub.class);
+  private static class RepositoryMockBuilder {
 
-    UsesGithubForDevelopment provider = new UsesGithubForDevelopment(github);
-    provider = spy(provider);
-    when(provider.cache()).thenReturn(new GitHubProjectValueCache());
+    GHRepository repository;
+    List<GHCommit> commits;
+    Set<Integer> passedChecks;
 
-    GHRepository repository = mock(GHRepository.class);
-    GitHubDataFetcher fetcher = mock(GitHubDataFetcher.class);
-    when(provider.gitHubDataFetcher()).thenReturn(fetcher);
+    private final List<Consumer<Boolean>> checks = Arrays.asList(
+        passed -> when(repository.getMirrorUrl())
+          .thenReturn(passed ? null : "https://other.com/org/project"),
+        passed -> when(repository.getDescription())
+          .thenReturn(passed ? "This is the main repository" : "This is a mirror"),
+        passed -> when(repository.hasIssues()).thenReturn(passed),
+        passed -> when(repository.hasPages()).thenReturn(passed),
+        passed -> when(repository.hasWiki()).thenReturn(passed),
+        passed -> when(repository.isAllowMergeCommit()).thenReturn(passed),
+        passed -> when(repository.isArchived()).thenReturn(!passed),
+        passed -> when(repository.getSvnUrl())
+            .thenReturn(passed ? null : "svn://other.com/org/project"),
+        passed -> {
+          try {
+            commits.clear();
+            GHCommit commit = mock(GHCommit.class);
+            if (passed) {
+              GHUser user = mock(GHUser.class);
+              when(commit.getCommitter()).thenReturn(user);
+              when(commit.getAuthor()).thenReturn(user);
+            } else {
+              when(commit.getCommitter()).thenThrow(new IOException());
+            }
+            commits.add(commit);
+          } catch (IOException e) {
+            throw new UncheckedIOException("This should not happen", e);
+          }
+        }
+    );
 
-    GitHubProject project = mock(GitHubProject.class);
-    when(fetcher.repositoryFor(project, github)).thenReturn(repository);
-    when(repository.getMirrorUrl()).thenReturn("https://other.scm.com/org/original_repository");
+    RepositoryMockBuilder() {
+      init();
+    }
 
-    ValueHashSet values = new ValueHashSet();
-    assertEquals(0, values.size());
+    final void init() {
+      repository = mock(GHRepository.class);
+      commits = new ArrayList<>();
+      passedChecks = new HashSet<>();
+      checks.forEach(check -> check.accept(false));
+    }
 
-    provider.update(project, values);
+    int allChecks() {
+      return checks.size();
+    }
 
-    assertEquals(1, values.size());
-    assertTrue(values.has(USES_GITHUB_FOR_DEVELOPMENT));
-    assertTrue(values.of(USES_GITHUB_FOR_DEVELOPMENT).isPresent());
-    assertFalse(values.of(USES_GITHUB_FOR_DEVELOPMENT).get().isUnknown());
-    assertEquals(USES_GITHUB_FOR_DEVELOPMENT.value(false),
-        values.of(USES_GITHUB_FOR_DEVELOPMENT).get());
+    int passedChecks() {
+      return passedChecks.size();
+    }
+
+    void passCheck(int i) {
+      checks.get(i).accept(true);
+      passedChecks.add(i);
+    }
+
+    GHRepository repository() {
+      return repository;
+    }
+
+    List<GHCommit> commits() {
+      return commits;
+    }
+
   }
 
   @Test
-  public void testWithoutMirrorUrl() throws IOException {
-    GitHub github = mock(GitHub.class);
+  public void testVariousChecks() {
+    Random random = new Random();
+    random.setSeed(0);
 
-    UsesGithubForDevelopment provider = new UsesGithubForDevelopment(github);
-    provider = spy(provider);
-    when(provider.cache()).thenReturn(new GitHubProjectValueCache());
+    RepositoryMockBuilder builder = new RepositoryMockBuilder();
 
-    GHRepository repository = mock(GHRepository.class);
-    GitHubDataFetcher fetcher = mock(GitHubDataFetcher.class);
-    when(provider.gitHubDataFetcher()).thenReturn(fetcher);
-
-    GitHubProject project = mock(GitHubProject.class);
-    when(fetcher.repositoryFor(project, github)).thenReturn(repository);
-    when(repository.getMirrorUrl()).thenReturn(null);
-
-    ValueHashSet values = new ValueHashSet();
-    assertEquals(0, values.size());
-
-    provider.update(project, values);
-
-    assertEquals(1, values.size());
-    assertTrue(values.has(USES_GITHUB_FOR_DEVELOPMENT));
-    assertTrue(values.of(USES_GITHUB_FOR_DEVELOPMENT).isPresent());
-    assertFalse(values.of(USES_GITHUB_FOR_DEVELOPMENT).get().isUnknown());
-    assertEquals(USES_GITHUB_FOR_DEVELOPMENT.value(true),
-        values.of(USES_GITHUB_FOR_DEVELOPMENT).get());
+    int i = 0;
+    while (i < builder.allChecks()) {
+      builder.init();
+      random.ints(i, 0, builder.allChecks())
+          .forEach(builder::passCheck);
+      boolean expected = builder.passedChecks() / builder.allChecks() >= CONFIDENCE_THRESHOLD;
+      assertEquals(
+          expected,
+          UsesGithubForDevelopment.usesGitHubForDevelopment(
+              builder.repository(), builder.commits(), CONFIDENCE_THRESHOLD));
+      i++;
+    }
   }
 
   @Test
-  public void testWithEmptyMirrorUrl() throws IOException {
-    GitHub github = mock(GitHub.class);
+  public void testAllPassedChecks() {
+    RepositoryMockBuilder builder = new RepositoryMockBuilder();
+    for (int i = 0; i < builder.allChecks(); i++) {
+      builder.passCheck(i);
+    }
+    assertEquals(builder.allChecks(), builder.passedChecks());
+    assertTrue(
+        UsesGithubForDevelopment.usesGitHubForDevelopment(
+            builder.repository(), builder.commits(), 0.99));
+  }
 
-    UsesGithubForDevelopment provider = new UsesGithubForDevelopment(github);
-    provider = spy(provider);
-    when(provider.cache()).thenReturn(new GitHubProjectValueCache());
-
-    GHRepository repository = mock(GHRepository.class);
-    GitHubDataFetcher fetcher = mock(GitHubDataFetcher.class);
-    when(provider.gitHubDataFetcher()).thenReturn(fetcher);
-
-    GitHubProject project = mock(GitHubProject.class);
-    when(fetcher.repositoryFor(project, github)).thenReturn(repository);
-    when(repository.getMirrorUrl()).thenReturn(StringUtils.EMPTY);
-
-    ValueHashSet values = new ValueHashSet();
-    assertEquals(0, values.size());
-
-    provider.update(project, values);
-
-    assertEquals(1, values.size());
-    assertTrue(values.has(USES_GITHUB_FOR_DEVELOPMENT));
-    assertTrue(values.of(USES_GITHUB_FOR_DEVELOPMENT).isPresent());
-    assertFalse(values.of(USES_GITHUB_FOR_DEVELOPMENT).get().isUnknown());
-    assertEquals(USES_GITHUB_FOR_DEVELOPMENT.value(true),
-        values.of(USES_GITHUB_FOR_DEVELOPMENT).get());
+  @Test
+  public void testAllFailedChecks() {
+    RepositoryMockBuilder builder = new RepositoryMockBuilder();
+    assertFalse(
+        UsesGithubForDevelopment.usesGitHubForDevelopment(
+            builder.repository(), builder.commits(), 0.01));
   }
 
   @Test
@@ -128,5 +164,12 @@ public class UsesGithubForDevelopmentTest {
     assertTrue(values.has(USES_GITHUB_FOR_DEVELOPMENT));
     assertTrue(values.of(USES_GITHUB_FOR_DEVELOPMENT).isPresent());
     assertTrue(values.of(USES_GITHUB_FOR_DEVELOPMENT).get().isUnknown());
+  }
+
+  @Before
+  @After
+  public void cleanup() {
+    GitHubDataFetcher.instance().repositoryCache().clear();
+    GitHubDataFetcher.instance().commitsCache().clear();
   }
 }
