3ad07350318e3735d9a046bfc5c3513170eec8fc
Artem Smotrakov <artem.smotrakov@sap.com>
2020-05-04 14:18:36 +0200

    Update UsesOwaspDependencyCheck to support Gradle
    - Updated UsesOwaspDependencyCheck to read build.gradle file
      and check if OWASP Dependency Check plugin is used
    - Updated UsesOwaspDependencyCheckTest
    
    This fixes #131
    This fixes #11

--- src/test/java/com/sap/sgs/phosphor/fosstars/data/github/UsesOwaspDependencyCheckTest.java
+++ src/test/java/com/sap/sgs/phosphor/fosstars/data/github/UsesOwaspDependencyCheckTest.java
@@ -1,90 +1,105 @@
 package com.sap.sgs.phosphor.fosstars.data.github;
 
-import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.SCANS_FOR_VULNERABLE_DEPENDENCIES;
+import static com.sap.sgs.phosphor.fosstars.data.github.UsesOwaspDependencyCheck.USES_OWASP_DEPENDENCY_CHECK;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import com.sap.sgs.phosphor.fosstars.data.Terminal;
 import com.sap.sgs.phosphor.fosstars.model.Value;
+import com.sap.sgs.phosphor.fosstars.model.ValueSet;
 import com.sap.sgs.phosphor.fosstars.model.value.ValueHashSet;
 import com.sap.sgs.phosphor.fosstars.tool.github.GitHubProject;
+import com.sap.sgs.phosphor.fosstars.tool.github.GitHubProjectValueCache;
 import java.io.IOException;
-import java.util.Optional;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
+import java.io.InputStream;
 import org.junit.Test;
+import org.kohsuke.github.GHContent;
+import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
-/**
- * TODO: This test fails when GitHub API rate limit exceeded or some networking issues occur.
- *       The test needs to be re-written to use mocks.
- *       Meanwhile, the test is ignored.
- */
-@Ignore
 public class UsesOwaspDependencyCheckTest {
 
-  private static GitHub github;
+  @Test
+  public void testMavenWithOwaspDependencyCheckInBuild() throws IOException {
+    try (InputStream is = getClass()
+        .getResourceAsStream("MavenWithOwaspDependencyCheckInBuild.xml")) {
 
-  @BeforeClass
-  public static void initGithubConnection() {
-    try {
-      github = GitHub.connectAnonymously();
-      github.checkApiUrlValidity();
-    } catch (Exception e) {
-      System.out.println("Couldn't connect to GitHub!");
-      e.printStackTrace();
-      github = null;
+      checkValue(createProvider(is, "pom.xml"), true);
     }
   }
 
   @Test
-  public void testApacheCxf() throws IOException {
-    assumeNotNull(github);
-    GitHubProject project = new GitHubProject("apache", "cxf");
-    UsesOwaspDependencyCheck provider = new UsesOwaspDependencyCheck(github);
-    ValueHashSet values = new ValueHashSet();
-    provider.set(new Terminal()).update(project, values);
-    assertTrue(values.has(SCANS_FOR_VULNERABLE_DEPENDENCIES));
-    Optional<Value> something = values.of(SCANS_FOR_VULNERABLE_DEPENDENCIES);
-    assertNotNull(something);
-    assertTrue(something.isPresent());
-    Value<Boolean> value = something.get();
-    assertNotNull(value);
-    assertEquals(Boolean.TRUE, value.get());
+  public void testMavenWithOwaspDependencyCheckInReporting() throws IOException {
+    try (InputStream is = getClass()
+        .getResourceAsStream("MavenWithOwaspDependencyCheckInReporting.xml")) {
+
+      checkValue(createProvider(is, "pom.xml"), true);
+    }
   }
 
   @Test
-  public void testApachePoi() throws IOException {
-    assumeNotNull(github);
-    GitHubProject project = new GitHubProject("apache", "poi");
-    UsesOwaspDependencyCheck provider = new UsesOwaspDependencyCheck(github);
-    ValueHashSet values = new ValueHashSet();
-    provider.set(new Terminal()).update(project, values);
-    assertTrue(values.has(SCANS_FOR_VULNERABLE_DEPENDENCIES));
-    Optional<Value> something = values.of(SCANS_FOR_VULNERABLE_DEPENDENCIES);
-    assertNotNull(something);
-    assertTrue(something.isPresent());
-    Value<Boolean> value = something.get();
-    assertNotNull(value);
-    assertEquals(Boolean.FALSE, value.get());
+  public void testMavenWithoutOwaspDependencyCheck() throws IOException {
+    try (InputStream is = getClass()
+        .getResourceAsStream("MavenWithoutOwaspDependencyCheck.xml")) {
+
+      checkValue(createProvider(is, "pom.xml"), false);
+    }
   }
 
   @Test
-  public void testSpringSecurityOAuth() throws IOException {
-    assumeNotNull(github);
-    GitHubProject project = new GitHubProject("spring-projects", "spring-security-oauth");
+  public void testGradleWithOwaspDependencyCheck() throws IOException {
+    try (InputStream is = getClass()
+        .getResourceAsStream("GradleWithOwaspDependencyCheck.gradle")) {
+
+      checkValue(createProvider(is, "build.gradle"), true);
+    }
+  }
+
+  @Test
+  public void testGradleWithoutOwaspDependencyCheck() throws IOException {
+    try (InputStream is = getClass()
+        .getResourceAsStream("GradleWithoutOwaspDependencyCheck.gradle")) {
+
+      checkValue(createProvider(is, "build.gradle"), false);
+    }
+  }
+
+  private static UsesOwaspDependencyCheck createProvider(InputStream is, String filename)
+      throws IOException {
+
+    GHContent content = mock(GHContent.class);
+    when(content.isFile()).thenReturn(true);
+    when(content.read()).thenReturn(is);
+
+    GHRepository repository = mock(GHRepository.class);
+    when(repository.getFileContent(filename)).thenReturn(content);
+
+    GitHub github = mock(GitHub.class);
+    when(github.getRepository(any())).thenReturn(repository);
+
     UsesOwaspDependencyCheck provider = new UsesOwaspDependencyCheck(github);
-    ValueHashSet values = new ValueHashSet();
-    provider.set(new Terminal()).update(project, values);
-    assertTrue(values.has(SCANS_FOR_VULNERABLE_DEPENDENCIES));
-    Optional<Value> something = values.of(SCANS_FOR_VULNERABLE_DEPENDENCIES);
-    assertNotNull(something);
-    assertTrue(something.isPresent());
-    Value<Boolean> value = something.get();
-    assertNotNull(value);
-    assertEquals(Boolean.FALSE, value.get());
+    provider.set(new GitHubProjectValueCache());
+    provider.gitHubDataFetcher().repositoryCache().clear();
+
+    return provider;
+  }
+
+  private static void checkValue(UsesOwaspDependencyCheck provider, boolean expectedValue)
+      throws IOException {
+
+    GitHubProject project = new GitHubProject("org", "test");
+
+    ValueSet values = new ValueHashSet();
+    provider.update(project, values);
+
+    assertEquals(1, values.size());
+    assertTrue(values.has(USES_OWASP_DEPENDENCY_CHECK));
+    assertTrue(values.of(USES_OWASP_DEPENDENCY_CHECK).isPresent());
+
+    Value<Boolean> value = values.of(USES_OWASP_DEPENDENCY_CHECK).get();
+    assertEquals(expectedValue, value.get());
   }
 
 }
