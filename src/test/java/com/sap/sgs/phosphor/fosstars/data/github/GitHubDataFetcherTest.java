8124caccb66c94c0c12bce9121cabbbef698001e
Artem Smotrakov <artem.smotrakov@sap.com>
2020-05-05 19:37:15 +0200

    Updated GitHubDataFetcher to pull updates to local repositories
    This is a part of #168

--- src/test/java/com/sap/sgs/phosphor/fosstars/data/github/GitHubDataFetcherTest.java
+++ src/test/java/com/sap/sgs/phosphor/fosstars/data/github/GitHubDataFetcherTest.java
@@ -1,7 +1,9 @@
 package com.sap.sgs.phosphor.fosstars.data.github;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -9,8 +11,14 @@ import static org.mockito.Mockito.when;
 import com.sap.sgs.phosphor.fosstars.TestGitHubDataFetcherHolder;
 import com.sap.sgs.phosphor.fosstars.tool.github.GitHubProject;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import org.junit.Test;
 import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
 public class GitHubDataFetcherTest extends TestGitHubDataFetcherHolder  {
 
@@ -44,5 +52,16 @@ public class GitHubDataFetcherTest extends TestGitHubDataFetcherHolder  {
     assertEquals(fetcher.repositoryCache().maxSize(), fetcher.repositoryCache().size());
 
     assertNotNull(fetcher.repositoryFor(latestProject));
+  }
+
+  @Test
+  public void testShouldUpdate() throws IOException {
+    GitHubDataFetcher fetcher = new GitHubDataFetcher(mock(GitHub.class));
+    Date now = Date.from(Instant.now());
+    Duration twoDays = Duration.ofDays(2);
+    fetcher.pullAfter(twoDays);
+    Date threeDaysAgo = Date.from(Instant.now().minus(3, ChronoUnit.DAYS));
+    assertTrue(fetcher.shouldUpdate(new LocalRepository(Paths.get("."), threeDaysAgo)));
+    assertFalse(fetcher.shouldUpdate(new LocalRepository(Paths.get("."), now)));
   }
 }
