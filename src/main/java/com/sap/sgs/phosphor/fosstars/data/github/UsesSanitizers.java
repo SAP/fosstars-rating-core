199dbc44bf5a9dd15ed27da2c6b9cdb52b58a315
Artem Smotrakov <artem.smotrakov@sap.com>
2020-05-08 19:45:44 +0200

    Make UsesSanitizers a bit smarter
    This is a part of #99

--- src/main/java/com/sap/sgs/phosphor/fosstars/data/github/UsesSanitizers.java
+++ src/main/java/com/sap/sgs/phosphor/fosstars/data/github/UsesSanitizers.java
@@ -88,15 +88,15 @@ public class UsesSanitizers extends GitHubCachingDataProvider {
 
       List<String> sanitizers = lookForSanitizers(content.get());
       for (String sanitizer : sanitizers) {
-        if (sanitizer.contains("address")) {
+        if (sanitizer.equals("address")) {
           values.update(USES_ADDRESS_SANITIZER.value(true));
         }
 
-        if (sanitizer.contains("memory")) {
+        if (sanitizer.equals("memory")) {
           values.update(USES_MEMORY_SANITIZER.value(true));
         }
 
-        if (sanitizer.contains("undefined")) {
+        if (sanitizer.equals("undefined")) {
           values.update(USES_UNDEFINED_BEHAVIOR_SANITIZER.value(true));
         }
       }
@@ -127,29 +127,66 @@ public class UsesSanitizers extends GitHubCachingDataProvider {
   }
 
   /**
-   * Looks for options that enable one of the sanitizers.
+   * Looks for options that enable sanitizers.
    *
    * @param content The content to be checked.
-   * @return A list of lines that contain options that enable the sanitizers.
+   * @return A list of options that enable sanitizers.
    * @throws IOException If something went wrong.
    */
   static List<String> lookForSanitizers(String content) throws IOException {
-    List<String> lines = new ArrayList<>();
+    List<String> options = new ArrayList<>();
 
     try (BufferedReader reader = new BufferedReader(new StringReader(content))) {
       String line;
       while ((line = reader.readLine()) != null) {
-        int start = line.indexOf(SANITIZER_OPTION);
-        if (start >= 0) {
-          int end = line.indexOf(" ", start);
-          if (end < 0) {
-            end = line.length();
-          }
-          lines.add(line.substring(start, end));
-        }
+        options.addAll(parse(line));
       }
     }
 
-    return lines;
+    return options;
+  }
+
+  /**
+   * Looks for options that enable sanitizers.
+   *
+   * @param line The string to be parsed.
+   * @return A list of options that enable sanitizers.
+   */
+  static List<String> parse(String line) {
+    List<String> options = new ArrayList<>();
+
+    line = line.trim();
+
+    int index = line.indexOf(SANITIZER_OPTION);
+    if (index < 0) {
+      return options;
+    }
+    line = line.substring(index);
+
+    String[] elements = line.split(SANITIZER_OPTION);
+    for (String element : elements) {
+      element = element.trim();
+      if (element.isEmpty()) {
+        continue;
+      }
+
+      // tokenize by a comma that may be surrounded by whitespaces
+      String[] values = element.split("\\s*,\\s*");
+      for (String value : values) {
+        value = value.trim();
+        if (value.isEmpty()) {
+          continue;
+        }
+
+        int spacePosition = value.indexOf(" ");
+        if (spacePosition >= 0) {
+          value = value.substring(0, spacePosition);
+        }
+
+        options.add(value);
+      }
+    }
+
+    return options;
   }
 }
