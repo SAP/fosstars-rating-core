a9a36888d53ce88cb8099e76f8f2438a58c990ec
Artem Smotrakov <artem.smotrakov@sap.com>
2020-05-11 12:23:31 +0200

    Added FuzzedInOssFuzz data provider
    - The new data provider fills out the FUZZED_IN_OSS_FUZZ feature.
    - Added the new data provider to the demo tool.
    - Added FuzzedInOssFuzzTest.
    
    This is a part of #98

---
+++ src/main/java/com/sap/sgs/phosphor/fosstars/data/github/FuzzedInOssFuzz.java
@@ -0,0 +1,60 @@
+package com.sap.sgs.phosphor.fosstars.data.github;
+
+import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.FUZZED_IN_OSS_FUZZ;
+
+import com.sap.sgs.phosphor.fosstars.model.Feature;
+import com.sap.sgs.phosphor.fosstars.model.Value;
+import com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures;
+import com.sap.sgs.phosphor.fosstars.tool.github.GitHubProject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.commons.io.IOUtils;
+
+/**
+ * This data provider check if an open-source project is included to the OSS-Fuzz project.
+ * It fills out the {@link OssFeatures#FUZZED_IN_OSS_FUZZ} feature.
+ */
+public class FuzzedInOssFuzz extends CachedSingleFeatureGitHubDataProvider {
+
+  /**
+   * OSS-Fuzz project on GitHub.
+   */
+  static final GitHubProject OSS_FUZZ_PROJECT = new GitHubProject("google", "oss-fuzz");
+
+  /**
+   * Initializes a data provider.
+   *
+   * @param fetcher An interface to GitHub.
+   */
+  public FuzzedInOssFuzz(GitHubDataFetcher fetcher) {
+    super(fetcher);
+  }
+
+  @Override
+  protected Feature supportedFeature() {
+    return FUZZED_IN_OSS_FUZZ;
+  }
+
+  @Override
+  protected Value<Boolean> fetchValueFor(GitHubProject project) throws IOException {
+    LocalRepository ossFuzzRepository = fetcher.localRepositoryFor(OSS_FUZZ_PROJECT);
+
+    List<Path> dockerFiles = Files.walk(ossFuzzRepository.info().path())
+        .filter(Files::isRegularFile)
+        .filter(path -> "Dockerfile".equals(path.getFileName().toString()))
+        .collect(Collectors.toList());
+
+    String url = project.url().toString();
+    for (Path dockerFile : dockerFiles) {
+      String content = IOUtils.toString(Files.newInputStream(dockerFile));
+      if (content.contains(url)) {
+        return FUZZED_IN_OSS_FUZZ.value(true);
+      }
+    }
+
+    return FUZZED_IN_OSS_FUZZ.value(false);
+  }
+}
