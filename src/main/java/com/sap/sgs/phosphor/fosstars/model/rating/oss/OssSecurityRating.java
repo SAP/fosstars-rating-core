40d30ce96b9b4fb0ed9989b2d4ee01df05129248
Artem Smotrakov <artem.smotrakov@sap.com>
2020-05-26 21:26:36 +0300

    Relative thresholds for labels in the OSS security rating
    - The OssSecurityRating can now be configured with thresholds
      for labels.
    - The thresholds are calculated by the SecurityRatingAnalysis
      notepad and stored to a JSON file.
    - The RatingRepository reads the thresholds from the file
      and uses them in the open-source security rating.
    
    This fixes #188

--- src/main/java/com/sap/sgs/phosphor/fosstars/model/rating/oss/OssSecurityRating.java
+++ src/main/java/com/sap/sgs/phosphor/fosstars/model/rating/oss/OssSecurityRating.java
@@ -6,25 +6,48 @@ import com.sap.sgs.phosphor.fosstars.model.Label;
 import com.sap.sgs.phosphor.fosstars.model.Score;
 import com.sap.sgs.phosphor.fosstars.model.rating.AbstractRating;
 import com.sap.sgs.phosphor.fosstars.model.score.oss.OssSecurityScore;
+import java.util.Objects;
 
 /**
  * This is a security rating for open-source projects
- * which is based on a security score for open-source project.
+ * that is based on a security score for open-source project.
  */
 public class OssSecurityRating extends AbstractRating {
 
+  /**
+   * A set of labels for the rating.
+   */
   public enum SecurityLabel implements Label {
+
     BAD, MODERATE, GOOD
   }
 
   /**
-   * Initializes a security rating based on the security score for open-source projects.
+   * Thresholds for labels.
+   */
+  private final Thresholds thresholds;
+
+  /**
+   * Initializes a security rating with defaults.
+   */
+  public OssSecurityRating() {
+    this(new OssSecurityScore(), Thresholds.DEFAULT);
+  }
+
+  /**
+   * Initializes a security rating based on a security score for open-source projects.
+   *
+   * @param score The security score.
+   * @param thresholds Thresholds for labels.
    */
   @JsonCreator
   public OssSecurityRating(
-      @JsonProperty("score") OssSecurityScore score) {
+      @JsonProperty("score") OssSecurityScore score,
+      @JsonProperty("thresholds") Thresholds thresholds) {
 
     super("Security rating for open-source projects", score);
+    Objects.requireNonNull(thresholds, "Oh no! Thresholds is null!");
+    this.thresholds = thresholds;
   }
 
   @Override
@@ -39,15 +62,58 @@ public class OssSecurityRating extends AbstractRating {
   protected SecurityLabel label(double score) {
     Score.check(score);
 
-    if (score < 5.0) {
+    if (score < thresholds.moderate) {
       return SecurityLabel.BAD;
     }
 
-    if (score < 8.0) {
+    if (score < thresholds.good) {
       return SecurityLabel.MODERATE;
     }
 
     return SecurityLabel.GOOD;
+  }
+
+  /**
+   * Holds thresholds for labels.
+   */
+  public static class Thresholds {
+
+    /**
+     * The default thresholds.
+     */
+    public static final Thresholds DEFAULT = new Thresholds(5.0, 8.0);
+
+    /**
+     * A threshold for the moderate label.
+     */
+    private final double moderate;
+
+    /**
+     * A threshold for the good label.
+     */
+    private final double good;
+
+    /**
+     * Initialize thresholds.
+     *
+     * @param moderate A threshold for the moderate label.
+     * @param good A threshold for the good label.
+     */
+    @JsonCreator
+    public Thresholds(
+        @JsonProperty("moderate") double moderate, @JsonProperty("good") double good) {
+
+      Score.check(moderate);
+      Score.check(good);
+
+      if (moderate >= good) {
+        throw new IllegalArgumentException(
+            "On ho! The moderate threshold is greater or equal to the good one!");
+      }
+
+      this.moderate = moderate;
+      this.good = good;
+    }
   }
 
 }
