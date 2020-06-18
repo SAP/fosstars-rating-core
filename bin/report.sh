a1b2d8fd2234e2a88dac84da5dbb2ae049431fd4
Artem Smotrakov <artem.smotrakov@sap.com>
2020-06-18 17:12:33 +0300

    Added a script for updating ratings for well-known projects
    This is a part of #228

---
+++ bin/report.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ "${TOKEN}" = "" ]; then
+  echo "Achtung! No GitHub token! Set TOKEN environment variable!"
+  exit 1
+fi
+
+set -e
+
+if [ "${BUILD}" = "yes" ]; then
+  mvn clean package -DskipTests
+fi
+
+if [ "${CLEAN}" = "yes" ]; then
+  rm -rf .fosstars/github_project_value_cache.json
+  rm -rf .fosstars/project_rating_cache.json
+  echo "[]" > docs/oss/security/github_projects.json
+
+  for file in $(find docs/oss/security -name "*.md" | grep -v README)
+  do
+    rm $file
+  done
+fi
+
+configs="other.yml jackson.yml spring.yml apache.yml eclipse.yml"
+
+echo "" > report.log
+for config in ${configs}
+do
+  java -jar target/fosstars-github-rating-calc.jar \
+      --no-questions --token ${TOKEN} --config docs/oss/security/${config} 2>&1 | tee report.log
+done
+
+if grep -i exception report.log > /dev/null 2>&1; then
+  echo "Achtung! Looks like there were some errors, check out report.log"
+fi
+
