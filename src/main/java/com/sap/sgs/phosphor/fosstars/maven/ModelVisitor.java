b4719bc8e78773678e1c0e6bf61a41f630db0c34
Artem Smotrakov <artem.smotrakov@sap.com>
2020-07-28 14:41:39 +0300

    Updated ModelVisitor to visit dependencies
    Part of #275

--- src/main/java/com/sap/sgs/phosphor/fosstars/maven/ModelVisitor.java
+++ src/main/java/com/sap/sgs/phosphor/fosstars/maven/ModelVisitor.java
@@ -1,6 +1,7 @@
 package com.sap.sgs.phosphor.fosstars.maven;
 
 import java.util.Set;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Plugin;
 import org.apache.maven.model.ReportPlugin;
 
@@ -13,7 +14,7 @@ public interface ModelVisitor {
    * Known locations of elements in a POM file.
    */
   enum Location {
-    BUILD, REPORTING, PROFILE, MANAGEMENT
+    BUILD, REPORTING, PROFILE, MANAGEMENT, DEPENDENCIES
   }
 
   /**
@@ -31,4 +32,12 @@ public interface ModelVisitor {
    * @param locations A set of locations that tells where the plugin is located.
    */
   void accept(ReportPlugin plugin, Set<Location> locations);
+
+  /**
+   * Visit a dependency.
+   *
+   * @param dependency The plugin.
+   * @param locations A set of locations that tells where the dependency is located.
+   */
+  void accept(Dependency dependency, Set<Location> locations);
 }
