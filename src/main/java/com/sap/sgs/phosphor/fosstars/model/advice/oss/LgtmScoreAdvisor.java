f9aaf040c4c7ee8778512dc12c9e66b45b426cf2
Artem Smotrakov <artem.smotrakov@sap.com>
2020-12-28 13:11:51 +0100

    Improved LgtmScoreAdvisor
    Part of #321

--- src/main/java/com/sap/sgs/phosphor/fosstars/model/advice/oss/LgtmScoreAdvisor.java
+++ src/main/java/com/sap/sgs/phosphor/fosstars/model/advice/oss/LgtmScoreAdvisor.java
@@ -2,8 +2,10 @@ package com.sap.sgs.phosphor.fosstars.model.advice.oss;
 
 import static com.sap.sgs.phosphor.fosstars.model.feature.oss.OssFeatures.WORST_LGTM_GRADE;
 import static com.sap.sgs.phosphor.fosstars.model.other.Utils.findValue;
+import static com.sap.sgs.phosphor.fosstars.model.value.LgtmGrade.A_PLUS;
 
 import com.sap.sgs.phosphor.fosstars.model.Advice;
+import com.sap.sgs.phosphor.fosstars.model.Value;
 import com.sap.sgs.phosphor.fosstars.model.score.oss.LgtmScore;
 import com.sap.sgs.phosphor.fosstars.model.value.LgtmGrade;
 import com.sap.sgs.phosphor.fosstars.model.value.ScoreValue;
@@ -14,7 +16,7 @@ import java.util.stream.Collectors;
 /**
  * An advisor for {@link LgtmScore}.
  */
-public class LgtmScoreAdvisor extends AbstractOssScoreAdvisor {
+public class LgtmScoreAdvisor extends AbstractOssScoreAdvisor<LgtmScore> {
 
   /**
    * Create a new advisor.
@@ -26,12 +28,32 @@ public class LgtmScoreAdvisor extends AbstractOssScoreAdvisor {
   @Override
   List<Advice> adviseFor(ScoreValue scoreValue, AdviceFactory factory) {
     return findValue(scoreValue.usedFeatureValues(), WORST_LGTM_GRADE)
-        .filter(worseLgtmGrade -> !worseLgtmGrade.isUnknown())
-        .filter(worseLgtmGrade -> worseLgtmGrade.get() != LgtmGrade.A_PLUS)
+        .filter(LgtmScoreAdvisor::isKnown)
+        .filter(LgtmScoreAdvisor::notTheBest)
         .map(value -> adviceStorage.advicesFor(value.feature())
             .stream()
             .map(content -> factory.createAdvice(value, content))
             .collect(Collectors.toList()))
         .orElse(Collections.emptyList());
+  }
+
+  /**
+   * Checks if a value is known.
+   *
+   * @param value The value to be checked.
+   * @return True if the value is known, false otherwise.
+   */
+  private static boolean isKnown(Value<LgtmGrade> value) {
+    return !value.isUnknown();
+  }
+
+  /**
+   * Check if an LGTM grade from a value is not the best one.
+   *
+   * @param value THe value to be checked.
+   * @return True if the LGTM grade is not the best one, false otherwise.
+   */
+  private static boolean notTheBest(Value<LgtmGrade> value) {
+    return value.get() != A_PLUS;
   }
 }
