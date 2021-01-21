736ee56984b3afac7ab3a1b5c3c0fa39393ac8f4
Artem Smotrakov <artem.smotrakov@sap.com>
2021-01-21 13:46:27 +0100

    SVG badges for GitHub action
    Part of #401

--- .github/actions/fosstars-create-single-report/entrypoint.sh
+++ .github/actions/fosstars-create-single-report/entrypoint.sh
@@ -15,14 +15,29 @@ if [ $? -ne 0 ]; then
 fi
 
 # Generate a report
+report_file="fosstars_security_rating.txt"
 java -jar /opt/stuff/fosstars-rating-core/target/fosstars-github-rating-calc.jar \
-          --verbose --url $PROJECT_SCM_URL --token $TOKEN 2>&1 | tee fosstars_security_rating.txt
+          --verbose --url $PROJECT_SCM_URL --token $TOKEN 2>&1 | tee $report_file
+
+label=$(cat fosstars_security_rating.txt | grep "Rating: " | cut -d ">" -f 2 | tr '[:upper:]' '[:lower:]' | sed 's/ //g')
+case $label in
+good|moderate|bad|unclear)
+  suffix=$label
+  ;;
+*)
+  suffix="unknown"
+  ;;
+esac
+
+current_badge_file="fosstars-security-rating.svg"
+wget -O $current_badge_file https://raw.githubusercontent.com/artem-smotrakov/fosstars-rating-core/fosstars-single-report-action/.github/actions/fosstars-create-single-report/images/security-fosstars-$suffix.svg
+git add $current_badge_file
 
 # Commit the report
 set -e
 git config --global user.name "Fosstars"
 git config --global user.email "fosstars@users.noreply.github.com"
 git remote set-url origin https://x-access-token:$TOKEN@github.com/$GITHUB_REPOSITORY
-git add fosstars_security_rating.txt
-git commit -m "Update Fosstars report" fosstars_security_rating.txt
+git add $report_file
+git commit -m "Update Fosstars security rating report" $report_file $current_badge_file
 git push origin $REPORT_BRANCH
