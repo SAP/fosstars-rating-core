869fd119e58c38438345f8fe204e59674217a0f7
Artem Smotrakov <artem.smotrakov@sap.com>
2021-01-27 13:49:44 +0100

    Restore saved locale in PrettyPrinterTest
    Fixes #407

--- src/test/java/com/sap/oss/phosphor/fosstars/tool/format/PrettyPrinterTest.java
+++ src/test/java/com/sap/oss/phosphor/fosstars/tool/format/PrettyPrinterTest.java
@@ -58,6 +58,7 @@ import com.sap.oss.phosphor.fosstars.model.value.Vulnerabilities;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Set;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -102,8 +103,11 @@ public class PrettyPrinterTest {
       OWASP_DEPENDENCY_CHECK_FAIL_CVSS_THRESHOLD.notSpecifiedValue(),
       PACKAGE_MANAGERS.value(new PackageManagers(MAVEN)));
 
+  private static Locale savedLocale;
+
   @BeforeClass
   public static void setup() {
+    savedLocale = Locale.getDefault();
     Locale.setDefault(new Locale("en", "US"));
   }
 
@@ -166,4 +170,10 @@ public class PrettyPrinterTest {
     assertEquals("9.0  out of 10.0",
         PrettyPrinter.printValueAndMax(9.0, 10.0));
   }
+
+  @AfterClass
+  public static void cleanup() {
+    Locale.setDefault(savedLocale);
+  }
+
 }
