68524e92b88e40d4527b5ab3132c33a7b95bb162
Artem Smotrakov <artem.smotrakov@sap.com>
2021-03-23 12:34:16 +0300

    Added an advisor for NoHttp tool
    - Added NoHttpAdvisor
    - Added NoHttpAdvisorTest
    
    Fixes #383

---
+++ src/main/java/com/sap/oss/phosphor/fosstars/advice/oss/NoHttpAdvisor.java
@@ -0,0 +1,45 @@
+package com.sap.oss.phosphor.fosstars.advice.oss;
+
+import static com.sap.oss.phosphor.fosstars.model.feature.oss.OssFeatures.USES_NOHTTP;
+
+import com.sap.oss.phosphor.fosstars.advice.Advice;
+import com.sap.oss.phosphor.fosstars.advice.oss.OssAdviceContentYamlStorage.OssAdviceContext;
+import com.sap.oss.phosphor.fosstars.model.Subject;
+import com.sap.oss.phosphor.fosstars.model.Value;
+import java.net.MalformedURLException;
+import java.util.List;
+
+/**
+ * An advisor for features related to NoHttp tool.
+ */
+public class NoHttpAdvisor extends AbstractOssAdvisor {
+
+  /**
+   * Create a new advisor.
+   *
+   * @param contextFactory A factory that provides contexts for advice.
+   */
+  public NoHttpAdvisor(OssAdviceContextFactory contextFactory) {
+    super(OssAdviceContentYamlStorage.DEFAULT, contextFactory);
+  }
+
+  @Override
+  protected List<Advice> adviseFor(
+      Subject subject, List<Value<?>> usedValues, OssAdviceContext context)
+      throws MalformedURLException {
+
+    return adviseForFeature(usedValues, USES_NOHTTP, subject, context, NoHttpAdvisor::noHttpTool);
+  }
+
+  /**
+   * Checks if a value tells that NoHttp tool is not used.
+   *
+   * @param value The value.
+   * @return True if NoHttp tool is not used, false otherwise.
+   */
+  private static boolean noHttpTool(Value<?> value) {
+    return USES_NOHTTP.equals(value.feature())
+        && !value.isUnknown()
+        && Boolean.FALSE.equals(value.get());
+  }
+}
