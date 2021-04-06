aa92f30de72c90aece4842ddf2f1051c674407fc
Artem Smotrakov <artem.smotrakov@sap.com>
2021-04-06 15:56:07 +0300

    Print out all rules in OssRulesOfPlayRatingMarkdownFormatter
    Part of #466

--- src/main/java/com/sap/oss/phosphor/fosstars/tool/format/OssRulesOfPlayRatingMarkdownFormatter.java
+++ src/main/java/com/sap/oss/phosphor/fosstars/tool/format/OssRulesOfPlayRatingMarkdownFormatter.java
@@ -5,6 +5,7 @@ import static com.sap.oss.phosphor.fosstars.model.score.oss.OssRulesOfPlayScore.
 import com.sap.oss.phosphor.fosstars.model.Confidence;
 import com.sap.oss.phosphor.fosstars.model.Subject;
 import com.sap.oss.phosphor.fosstars.model.Value;
+import com.sap.oss.phosphor.fosstars.model.value.BooleanValue;
 import com.sap.oss.phosphor.fosstars.model.value.RatingValue;
 import com.sap.oss.phosphor.fosstars.model.value.ScoreValue;
 import java.util.List;
@@ -55,25 +56,73 @@ public class OssRulesOfPlayRatingMarkdownFormatter extends CommonFormatter {
         .replace("%STATUS%", ratingValue.label().name())
         .replace("%CONFIDENCE_LABEL%", confidenceLabelFor(ratingValue.confidence()))
         .replace("%CONFIDENCE_VALUE%", formatted(ratingValue.confidence()))
-        .replace("%VIOLATED_RULES%", violatedRules(scoreValue));
+        .replace("%VIOLATED_RULES%", violatedRulesFrom(scoreValue))
+        .replace("%PASSED_RULES%", passedRulesFrom(scoreValue))
+        .replace("%UNCLEAR_RULES%", unclearRulesFrom(scoreValue));
   }
 
   /**
-   * Prints violated rules.
+   * Prints out violated rules.
    *
    * @param scoreValue A score value.
    * @return A formatted list of violated rules.
    */
-  private String violatedRules(ScoreValue scoreValue) {
+  private String violatedRulesFrom(ScoreValue scoreValue) {
     List<Value<Boolean>> violatedRules = findViolatedRules(scoreValue.usedValues());
 
     if (violatedRules.isEmpty()) {
-      return "No violated rules.";
+      return StringUtils.EMPTY;
     }
 
-    return violatedRules.stream()
-        .map(OssRulesOfPlayRatingMarkdownFormatter::formatViolatedRule)
+    String content = violatedRules.stream()
+        .map(OssRulesOfPlayRatingMarkdownFormatter::formatRule)
         .collect(Collectors.joining("\n"));
+
+    return String.format("## Violated rules%n%n%s%n", content);
+  }
+
+  /**
+   * Prints out passed rules.
+   *
+   * @param scoreValue A score value.
+   * @return A formatted list of passed rules.
+   */
+  private String passedRulesFrom(ScoreValue scoreValue) {
+    List<Value<Boolean>> violatedRules = findViolatedRules(scoreValue.usedValues());
+
+    String content = scoreValue.usedValues().stream()
+        .filter(rule -> !violatedRules.contains(rule))
+        .filter(rule -> !rule.isUnknown())
+        .map(BooleanValue.class::cast)
+        .map(OssRulesOfPlayRatingMarkdownFormatter::formatRule)
+        .collect(Collectors.joining("\n"));
+
+    if (content.trim().isEmpty()) {
+      return StringUtils.EMPTY;
+    }
+
+    return String.format("## Passed rules%n%n%s%n", content);
+  }
+
+  /**
+   * Prints out unclear rules.
+   *
+   * @param scoreValue A score value.
+   * @return A formatted list of unclear rules.
+   */
+  private String unclearRulesFrom(ScoreValue scoreValue) {
+    List<Value<?>> unclearRules = scoreValue.usedValues().stream()
+        .filter(Value::isUnknown).collect(Collectors.toList());
+
+    if (unclearRules.isEmpty()) {
+      return StringUtils.EMPTY;
+    }
+
+    String content = unclearRules.stream()
+        .map(value -> String.format("1.  %s", nameOf(value.feature())))
+        .collect(Collectors.joining("\n"));
+
+    return String.format("## Unclear rules%n%n%s%n", content);
   }
 
   /**
@@ -82,7 +131,7 @@ public class OssRulesOfPlayRatingMarkdownFormatter extends CommonFormatter {
    * @param value The rule.
    * @return A formatted violated rule.
    */
-  private static String formatViolatedRule(Value<Boolean> value) {
-    return String.format("1.  %s %s", nameOf(value.feature()), value.get() ? "Yes" : "No");
+  private static String formatRule(Value<Boolean> value) {
+    return String.format("1.  %s **%s**", nameOf(value.feature()), value.get() ? "Yes" : "No");
   }
 }
