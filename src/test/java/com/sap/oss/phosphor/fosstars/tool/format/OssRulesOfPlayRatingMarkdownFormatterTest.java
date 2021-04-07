a21e0bb7cbb4c44f3470645246b730aaeac3305d
Artem Smotrakov <artem.smotrakov@sap.com>
2021-04-07 17:26:52 +0300

    OssRulesOfPlayRatingMarkdownFormatter should load custom rule IDs
    Part of #466

--- src/test/java/com/sap/oss/phosphor/fosstars/tool/format/OssRulesOfPlayRatingMarkdownFormatterTest.java
+++ src/test/java/com/sap/oss/phosphor/fosstars/tool/format/OssRulesOfPlayRatingMarkdownFormatterTest.java
@@ -19,7 +19,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class OssRulesOfPlayRatingMarkdownFormatterTest {
@@ -30,87 +29,108 @@ public class OssRulesOfPlayRatingMarkdownFormatterTest {
   private static final Path CONFIG_PATH
       = Paths.get("OssRulesOfPlayRatingMarkdownFormatter.config.yml");
 
-  @BeforeClass
-  public static void setup() throws IOException {
-    String content = "---\n"
-        + "ruleIds:\n"
-        + "  rl-license_file-1: If a project has a license\n"
-        + "  rl-license_file-2: If a project uses an allowed license\n"
-        + "  rl-license_file-3: If a license has disallowed text\n"
-        + "  rl-readme_file-1: If a project has a README file";
-    Files.write(CONFIG_PATH, content.getBytes());
+  private static final String RULE_IDS =
+        "---\n"
+      + "ruleIds:\n"
+      + "  rl-license_file-1: If a project has a license\n"
+      + "  rl-license_file-2: If a project uses an allowed license\n"
+      + "  rl-license_file-3: If a license has disallowed text\n"
+      + "  rl-readme_file-1: If a project has a README file";
+
+  @Test
+  public void testPrintWithCompliantProject() throws IOException {
+    Files.write(CONFIG_PATH, RULE_IDS.getBytes());
+    try {
+      RatingValue ratingValue = RATING.calculate(allRulesPassed());
+      assertEquals(OssRulesOfPlayLabel.PASSED, ratingValue.label());
+      OssRulesOfPlayRatingMarkdownFormatter formatter
+          = new OssRulesOfPlayRatingMarkdownFormatter(CONFIG_PATH);
+      String text = formatter.print(ratingValue);
+      assertNotNull(text);
+      assertFalse(text.isEmpty());
+      String lowerCaseText = text.toLowerCase();
+      assertTrue(lowerCaseText.contains("status"));
+      assertTrue(lowerCaseText.contains("pass"));
+      assertFalse(lowerCaseText.contains("fail"));
+      assertFalse(lowerCaseText.contains("unclear"));
+      assertFalse(lowerCaseText.contains("unknown"));
+      checkRuleIds(text);
+    } finally {
+      FileUtils.forceDeleteOnExit(CONFIG_PATH.toFile());
+    }
   }
 
   @Test
-  public void testPrintWithCompliantProject() {
-    RatingValue ratingValue = RATING.calculate(allRulesPassed());
-    assertEquals(OssRulesOfPlayLabel.PASSED, ratingValue.label());
-    OssRulesOfPlayRatingMarkdownFormatter formatter = new OssRulesOfPlayRatingMarkdownFormatter();
-    String text = formatter.print(ratingValue);
-    assertNotNull(text);
-    assertFalse(text.isEmpty());
-    String lowerCaseText = text.toLowerCase();
-    assertTrue(lowerCaseText.contains("status"));
-    assertTrue(lowerCaseText.contains("pass"));
-    assertFalse(lowerCaseText.contains("fail"));
-    assertFalse(lowerCaseText.contains("unclear"));
-    assertFalse(lowerCaseText.contains("unknown"));
-    checkRuleIds(text);
+  public void testPrintWithWarnings() throws IOException {
+    Files.write(CONFIG_PATH, RULE_IDS.getBytes());
+    try {
+      ValueSet values = allRulesPassed();
+      values.update(OssRulesOfPlayScore.RECOMMENDED_FALSE.iterator().next().value(true));
+      RatingValue ratingValue = RATING.calculate(values);
+      assertEquals(OssRulesOfPlayLabel.PASSED_WITH_WARNING, ratingValue.label());
+      OssRulesOfPlayRatingMarkdownFormatter formatter
+          = new OssRulesOfPlayRatingMarkdownFormatter(CONFIG_PATH);
+      String text = formatter.print(ratingValue);
+      assertNotNull(text);
+      assertFalse(text.isEmpty());
+      String lowerCaseText = text.toLowerCase();
+      assertTrue(lowerCaseText.contains("status"));
+      assertTrue(lowerCaseText.contains("pass"));
+      assertTrue(lowerCaseText.contains("warning"));
+      assertFalse(lowerCaseText.contains("fail"));
+      assertFalse(lowerCaseText.contains("unclear"));
+      assertFalse(lowerCaseText.contains("unknown"));
+      checkRuleIds(text);
+    } finally {
+      FileUtils.forceDeleteOnExit(CONFIG_PATH.toFile());
+    }
   }
 
   @Test
-  public void testPrintWithWarnings() {
-    ValueSet values = allRulesPassed();
-    values.update(OssRulesOfPlayScore.RECOMMENDED_FALSE.iterator().next().value(true));
-    RatingValue ratingValue = RATING.calculate(values);
-    assertEquals(OssRulesOfPlayLabel.PASSED_WITH_WARNING, ratingValue.label());
-    OssRulesOfPlayRatingMarkdownFormatter formatter = new OssRulesOfPlayRatingMarkdownFormatter();
-    String text = formatter.print(ratingValue);
-    assertNotNull(text);
-    assertFalse(text.isEmpty());
-    String lowerCaseText = text.toLowerCase();
-    assertTrue(lowerCaseText.contains("status"));
-    assertTrue(lowerCaseText.contains("pass"));
-    assertTrue(lowerCaseText.contains("warning"));
-    assertFalse(lowerCaseText.contains("fail"));
-    assertFalse(lowerCaseText.contains("unclear"));
-    assertFalse(lowerCaseText.contains("unknown"));
-    checkRuleIds(text);
+  public void testPrintWithNotCompliantProject() throws IOException {
+    Files.write(CONFIG_PATH, RULE_IDS.getBytes());
+    try {
+      ValueSet values = allRulesPassed();
+      values.update(OssRulesOfPlayScore.EXPECTED_FALSE.iterator().next().value(true));
+      RatingValue ratingValue = RATING.calculate(values);
+      assertEquals(OssRulesOfPlayLabel.FAILED, ratingValue.label());
+      OssRulesOfPlayRatingMarkdownFormatter formatter
+          = new OssRulesOfPlayRatingMarkdownFormatter(CONFIG_PATH);
+      String text = formatter.print(ratingValue);
+      assertNotNull(text);
+      assertFalse(text.isEmpty());
+      String lowerCaseText = text.toLowerCase();
+      assertTrue(lowerCaseText.contains("status"));
+      assertTrue(lowerCaseText.contains("fail"));
+      assertTrue(lowerCaseText.contains("violated"));
+      assertFalse(lowerCaseText.contains("unclear"));
+      assertFalse(lowerCaseText.contains("unknown"));
+      checkRuleIds(text);
+    } finally {
+      FileUtils.forceDeleteOnExit(CONFIG_PATH.toFile());
+    }
   }
 
   @Test
-  public void testPrintWithNotCompliantProject() {
-    ValueSet values = allRulesPassed();
-    values.update(OssRulesOfPlayScore.EXPECTED_FALSE.iterator().next().value(true));
-    RatingValue ratingValue = RATING.calculate(values);
-    assertEquals(OssRulesOfPlayLabel.FAILED, ratingValue.label());
-    OssRulesOfPlayRatingMarkdownFormatter formatter = new OssRulesOfPlayRatingMarkdownFormatter();
-    String text = formatter.print(ratingValue);
-    assertNotNull(text);
-    assertFalse(text.isEmpty());
-    String lowerCaseText = text.toLowerCase();
-    assertTrue(lowerCaseText.contains("status"));
-    assertTrue(lowerCaseText.contains("fail"));
-    assertTrue(lowerCaseText.contains("violated"));
-    assertFalse(lowerCaseText.contains("unclear"));
-    assertFalse(lowerCaseText.contains("unknown"));
-    checkRuleIds(text);
-  }
-
-  @Test
-  public void testPrintWithUnclearProject() {
-    RatingValue ratingValue = RATING.calculate(allUnknown(RATING.allFeatures()));
-    assertEquals(OssRulesOfPlayLabel.UNCLEAR, ratingValue.label());
-    OssRulesOfPlayRatingMarkdownFormatter formatter = new OssRulesOfPlayRatingMarkdownFormatter();
-    String text = formatter.print(ratingValue);
-    assertNotNull(text);
-    assertFalse(text.isEmpty());
-    String lowerCaseText = text.toLowerCase();
-    assertTrue(lowerCaseText.contains("status"));
-    assertTrue(lowerCaseText.contains("unclear"));
-    assertFalse(lowerCaseText.contains("pass"));
-    assertFalse(lowerCaseText.contains("fail"));
-    checkRuleIds(text);
+  public void testPrintWithUnclearProject() throws IOException {
+    Files.write(CONFIG_PATH, RULE_IDS.getBytes());
+    try {
+      RatingValue ratingValue = RATING.calculate(allUnknown(RATING.allFeatures()));
+      assertEquals(OssRulesOfPlayLabel.UNCLEAR, ratingValue.label());
+      OssRulesOfPlayRatingMarkdownFormatter formatter
+          = new OssRulesOfPlayRatingMarkdownFormatter(CONFIG_PATH);
+      String text = formatter.print(ratingValue);
+      assertNotNull(text);
+      assertFalse(text.isEmpty());
+      String lowerCaseText = text.toLowerCase();
+      assertTrue(lowerCaseText.contains("status"));
+      assertTrue(lowerCaseText.contains("unclear"));
+      assertFalse(lowerCaseText.contains("pass"));
+      assertFalse(lowerCaseText.contains("fail"));
+      checkRuleIds(text);
+    } finally {
+      FileUtils.forceDeleteOnExit(CONFIG_PATH.toFile());
+    }
   }
 
   private static void checkRuleIds(String text) {
