474356874160c9b87b5c6dc41b0e8f23f60de679
D067452 <sourabh.sarvotham.parkala@sap.com>
2021-06-07 16:22:46 +0200

    Fixing package-info.java description
    Part of #468

--- src/main/java/com/sap/oss/phosphor/fosstars/data/artifact/experimental/owasp/data/package-info.java
+++ src/main/java/com/sap/oss/phosphor/fosstars/data/artifact/experimental/owasp/data/package-info.java
@@ -1,4 +1,5 @@
 /**
- * The package contains data providers which fetch data from OWASP Dependency-Check.
+ * The package contains POJOs which was manually generated based on a sample OWASP Dependency-Check
+ * result.
  */
 package com.sap.oss.phosphor.fosstars.data.artifact.experimental.owasp.data;
