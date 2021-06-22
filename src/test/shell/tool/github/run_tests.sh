c0be9c8156c967bceb208275c0f33e60386482ea
Artem Smotrakov <artem.smotrakov@sap.com>
2021-06-22 10:59:17 +0200

    Better tests for --config and --url
    Part of #589

--- src/test/shell/tool/github/run_tests.sh
+++ src/test/shell/tool/github/run_tests.sh
@@ -3,6 +3,7 @@
 # debug output
 #set -x
 
+
 ME=$(basename "$0")
 TEST_DIRECTORY=$(dirname $0)
 echo "Test directory: $TEST_DIRECTORY"
@@ -16,13 +17,22 @@ OLD_DIRECTORY=$(pwd)
 cd $TEST_DIRECTORY
 rm -rf $TEST_REPORT > /dev/null 2>&1
 touch $TEST_REPORT
+
 n=$(ls -la test_*.sh | wc -l)
 echo "Found $n tests"
+selected_test=$1
+if [ "x$selected_test" != "x" ]; then
+  echo "Run only $selected_test"
+fi
 for test in test_*.sh; do
+  if [ "x$selected_test" != "x" -a "$selected_test" != "$test" ]; then
+    continue
+  fi
   log="${test}.log"
   printf "Running $test ... "
-  bash $test 2>&1 | tee $log
+  bash $test > $log 2>&1
   code=$?
+  cat $log
   if [ $code -eq 0 ]; then
     status="passed"
   elif [ $code -eq 70 ]; then
@@ -32,13 +42,14 @@ for test in test_*.sh; do
   fi
   echo $status
   echo "$status $test" >> $TEST_REPORT
+  rm tmp.log > /dev/null 2>&1
 done
 
 echo "Here are the results:"
 cat $TEST_REPORT
 
 code=0
-if grep failed $TEST_REPORT > /dev/null 2>&1; then
+if grep failed $TEST_REPORT > /dev/null 2>&1 ; then
   echo "Some test failed"
   code=1
 fi
---
+++ src/test/shell/tool/github/test_project_security_with_config.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+TOKEN_OPTION=""
+if [ "$TOKEN" != "" ]; then
+  TOKEN_OPTION="--token $TOKEN"
+fi
+
+JAVA="java"
+if [ "$JAVA_HOME" != "" ]; then
+  JAVA="$JAVA_HOME/bin/java"
+fi
+
+JAR=${JAR:-"target/fosstars-github-rating-calc.jar"}
+
+# remove cache
+find .fosstars -name "*cache*.json" | xargs rm -rf
+
+rm -rf .fosstars/report > /dev/null 2>&1
+mkdir -p .fosstars/report
+
+$JAVA -jar $JAR \
+  --rating security \
+  --config test_config.yml \
+  $TOKEN_OPTION > tmp.log 2>&1
+
+if [ $? != 0 ]; then
+  echo "Unexpected exit code"
+  exit 1
+fi
+
+cat tmp.log
+
+declare -a expected_strings=(
+  'Figuring out if the project belongs to the Eclipse Software Foundation'
+  'Figuring out how the project uses CodeQL'
+  'Figuring out how the project uses LGTM'
+  'Figuring out if the project uses OWASP security libraries'
+  'Checking how the project uses Dependabot'
+  'Figuring out if the project uses GitHub for development'
+  'Figuring out if the project uses sanitizers'
+  'Figuring out if the project uses FindSecBugs'
+  'Figuring out if the project signs jar files'
+  'Looking for vulnerabilities in the project'
+  'Figuring out if the project has any unpatched vulnerability'
+  'Looking for vulnerabilities in NVD'
+  'Counting how many stars the project has'
+  'Counting how many people contributed to the project in the last three months'
+  'Counting how many commits have been done in the last three months'
+  'Figuring out if the project belongs to the Apache Software Foundation'
+  'Figuring out if the project uses signed commits'
+  'Figuring out if the project has a security team'
+  'Figuring out if the project uses nohttp'
+  'Figuring out if the project uses OWASP Dependency Check'
+  'Figuring out if the project has a bug bounty program'
+  'Counting how many watchers the project has'
+  'Looking for programming languages that are used in the project'
+  'Figuring out if the project has a security policy'
+  'Figuring out if the project is fuzzed in OSS-Fuzz'
+  'Looking for package managers'
+  'Looking for programming languages that are used in the project'
+  'Figuring out if the project is supported by a company'
+  'Found 2 projects'
+  'https://github.com/netty/netty'
+  'https://github.com/FasterXML/jackson-databind'
+  'Starting calculating ratings'
+  'Storing info about projects to .fosstars/report/github_projects.json'
+  'Storing a report to .fosstars/report/README.md'
+  'Storing the cache to .fosstars/project_rating_cache.json'
+)
+
+source lib.sh
+check_expected_output "${expected_strings[@]}" | grep Failed
+if [ $? == 0 ]; then
+  echo "check_expected_output() failed"
+  exit 1
+fi
+
+if [ grep Exception tmp.log > /dev/null 2>&1 ]; then
+  echo "Exceptions found"
+  exit 1
+fi
+
+ls .fosstars/report/github_projects.json > /dev/null 2>&1 || exit 1
+ls .fosstars/report/netty/netty.md > /dev/null 2>&1 || exit 1
+ls .fosstars/report/FasterXML/jackson-databind.md > /dev/null 2>&1 || exit 1
+ls .fosstars/report/README.md > /dev/null 2>&1 || exit 1
