ba3db8144981222eac54809e9e3c53ae67bb33de
Artem Smotrakov <artem.smotrakov@sap.com>
2021-06-22 12:32:32 +0200

    Added test_artifact_security_with_npm.sh
    Part of #589

---
+++ src/test/shell/tool/github/test_artifact_security_with_npm.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+TOKEN_OPTION=""
+if [ "$TOKEN" != "" ]; then
+  TOKEN_OPTION="--token $TOKEN"
+fi
+
+JAVA="java"
+if [ "$JAVA_HOME" != "" ]; then
+  JAVA="$JAVA_HOME/bin/java"
+fi
+
+JAR=${JAR:-"target/fosstars-github-rating-calc.jar"}
+
+source lib.sh
+
+clean_cache
+
+$JAVA -jar $JAR \
+  --rating oss-artifact-security \
+  --npm jquery@3.6.0 \
+  --verbose \
+  $TOKEN_OPTION > tmp.log 2>&1
+
+if [ $? != 0 ]; then
+  cat tmp.log
+  echo "Unexpected exit code"
+  exit 1
+fi
+
+cat tmp.log
+
+check_expected_output "${artifact_security_default_expected_strings[@]}" | tee | grep Failed
+if [ $? == 0 ]; then
+  echo "check_expected_output() failed"
+  exit 1
+fi
+
+declare -a expected_strings=(
+  'pkg:npm/jquery@3.6.0'
+)
+
+check_expected_output "${expected_strings[@]}" | tee | grep Failed
+if [ $? == 0 ]; then
+  echo "check_expected_output() failed"
+  exit 1
+fi
+
+if [ grep Exception tmp.log > /dev/null 2>&1 ]; then
+  echo "Exceptions found"
+  exit 1
+fi
