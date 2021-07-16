e8730caa4dffc43c8487798bec72000c1736dbf9
Artem Smotrakov <artem.smotrakov@sap.com>
2021-07-16 13:04:57 +0200

    Estimate changes in a project after security review
    Part of #586

--- src/test/java/com/sap/oss/phosphor/fosstars/model/score/oss/SecurityReviewScoreTest.java
+++ src/test/java/com/sap/oss/phosphor/fosstars/model/score/oss/SecurityReviewScoreTest.java
@@ -1,5 +1,6 @@
 package com.sap.oss.phosphor.fosstars.model.score.oss;
 
+import static com.sap.oss.phosphor.fosstars.TestUtils.DELTA;
 import static com.sap.oss.phosphor.fosstars.model.Score.MAX;
 import static com.sap.oss.phosphor.fosstars.model.Score.MIN;
 import static com.sap.oss.phosphor.fosstars.model.feature.oss.OssFeatures.SECURITY_REVIEWS;
@@ -16,38 +17,46 @@ import org.junit.Test;
 
 public class SecurityReviewScoreTest {
 
-  private static final double DELTA = 0.01;
-
   @Test
   public void testPointsFor() {
     Instant now = Instant.now();
     assertEquals(
         MAX,
-        SecurityReviewScore.pointsFor(new SecurityReview(Date.from(now)), now),
+        SecurityReviewScore.pointsFor(
+            new SecurityReview(Date.from(now), 0.0),
+            now),
         DELTA);
 
     Instant lessThanYearAgo = now.minus(180, DAYS);
     assertEquals(
         MAX,
-        SecurityReviewScore.pointsFor(new SecurityReview(Date.from(lessThanYearAgo)), now),
+        SecurityReviewScore.pointsFor(
+            new SecurityReview(Date.from(lessThanYearAgo), 0.1),
+            now),
         DELTA);
 
     Instant lastYear = now.minus(400, DAYS);
     assertEquals(
         5.0,
-        SecurityReviewScore.pointsFor(new SecurityReview(Date.from(lastYear)), now),
+        SecurityReviewScore.pointsFor(
+            new SecurityReview(Date.from(lastYear), 0.2),
+            now),
         DELTA);
 
     Instant twoYearsAgo = now.minus(365 * 2 + 50, DAYS);
     assertEquals(
         3.33,
-        SecurityReviewScore.pointsFor(new SecurityReview(Date.from(twoYearsAgo)), now),
+        SecurityReviewScore.pointsFor(
+            new SecurityReview(Date.from(twoYearsAgo), 0.3),
+            now),
         DELTA);
 
     Instant threeYearsAgo = now.minus(365 * 3 + 50, DAYS);
     assertEquals(
         2.5,
-        SecurityReviewScore.pointsFor(new SecurityReview(Date.from(threeYearsAgo)), now),
+        SecurityReviewScore.pointsFor(
+            new SecurityReview(Date.from(threeYearsAgo), 0.5),
+            now),
         DELTA);
   }
 
@@ -61,8 +70,8 @@ public class SecurityReviewScoreTest {
     ScoreValue scoreValue = score.calculate(
         SECURITY_REVIEWS.value(
             new SecurityReviews(
-                new SecurityReview(Date.from(lastYear)),
-                new SecurityReview(Date.from(threeYearsAgo)))));
+                new SecurityReview(Date.from(lastYear), 0.1),
+                new SecurityReview(Date.from(threeYearsAgo), 0.4))));
 
     assertFalse(scoreValue.isUnknown());
     assertFalse(scoreValue.isNotApplicable());
