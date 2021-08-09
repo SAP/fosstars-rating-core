f2389543bf37b1c559a349d37c06e9f4737c6ffa
Artem Smotrakov <artem.smotrakov@sap.com>
2021-08-09 12:38:41 +0200

    Added AdoptedRiskLikelihoodFactor score
    - Added a new feature and a score
    - Added a test
    
    Part of #671

---
+++ src/main/java/com/sap/oss/phosphor/fosstars/model/score/oss/risk/AdoptedRiskLikelihoodFactor.java
@@ -0,0 +1,36 @@
+package com.sap.oss.phosphor.fosstars.model.score.oss.risk;
+
+import static com.sap.oss.phosphor.fosstars.model.feature.oss.risk.OssRiskFeatures.IS_ADOPTED;
+import static com.sap.oss.phosphor.fosstars.model.other.Utils.findValue;
+
+import com.sap.oss.phosphor.fosstars.model.Value;
+import com.sap.oss.phosphor.fosstars.model.score.FeatureBasedScore;
+import com.sap.oss.phosphor.fosstars.model.value.ScoreValue;
+
+/**
+ * This scoring function outputs a likelihood factor for security risk of open source project.
+ * The factor is based on whether the project is adopted by a team.
+ */
+public class AdoptedRiskLikelihoodFactor extends FeatureBasedScore {
+
+  /**
+   * Create a new scoring function.
+   */
+  public AdoptedRiskLikelihoodFactor() {
+    super("Likelihood factor of adoption of an open source project by a team", IS_ADOPTED);
+  }
+
+  @Override
+  public ScoreValue calculate(Value<?>... values) {
+    Value<Boolean> adopted = findValue(values, IS_ADOPTED,
+        "Hey! You have to tell whether the project has been adopted or not!");
+
+    ScoreValue scoreValue = scoreValue(MIN, adopted);
+
+    if (adopted.isUnknown()) {
+      return scoreValue.makeUnknown();
+    }
+
+    return scoreValue.set(adopted.get() ? MAX : MIN);
+  }
+}
