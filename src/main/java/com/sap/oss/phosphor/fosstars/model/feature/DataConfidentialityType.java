12ca3413df45c54a64dc88c312c9ac5313be794c
Artem Smotrakov <artem.smotrakov@sap.com>
2021-08-10 12:29:36 +0200

    Added DataConfidentialityRiskImpactFactor
    - Added a new score and feature
    - Added tests
    
    Part of #671

---
+++ src/main/java/com/sap/oss/phosphor/fosstars/model/feature/DataConfidentialityType.java
@@ -0,0 +1,9 @@
+package com.sap.oss.phosphor.fosstars.model.feature;
+
+/**
+ * The enum contains values that describe how confidential data is.
+ */
+public enum DataConfidentialityType {
+
+  TEST, PUBLIC, INTERNAL, CONFIDENTIAL, PERSONAL
+}
